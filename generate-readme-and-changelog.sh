057759b7271963132a01ef1e76164f45e62a68b6
David Fritzsche <david.fritzsche@mvua.de>
2019-06-21 17:39:22 +0200

    Generate README.rst and CHANGELOG.rst using the rst builder

--- generate-readme-and-changelog.sh
+++ generate-readme-and-changelog.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-sphinx-build -q -b rst -d build/readme docs/readme .
-sphinx-build -q -b rst -d build/changelog docs/changelog .
+sphinx-build -q -a -E -b rst -d build/readme docs/readme .
+sphinx-build -q -a -E -b rst -d build/changelog docs/changelog .
